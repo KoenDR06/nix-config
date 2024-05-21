cfa8b0bfbd2492b9dbc089b69e59c2b8b49299be
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-05-21 17:23:27 +0200

    .

--- machines/luna/configuration.nix
+++ machines/luna/configuration.nix
@@ -30,23 +30,13 @@
   boot.loader.systemd-boot.enable = true;  
   boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
 
-
   hardware.pulseaudio.enable = true;
-
   networking.networkmanager.enable = true;
 
   services.jack = {
     jackd.enable = true;
-    # support ALSA only programs via ALSA JACK PCM plugin
     alsa.enable = false;
-    # support ALSA only programs via loopback device (supports programs like Steam)
-    loopback = {
-      enable = true;
-      # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-      #dmixConfig = ''
-      #  period_size 2048
-      #'';
-    };
+    loopback.enable = true;
   };
 
   users.extraUsers.horseman.extraGroups = [ "jackaudio" ];
