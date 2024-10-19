c9f6fb77d0993c93dc7fdb3b4f5d25c270a59c01
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-10-19 23:05:36 +0200

    Added a switch to ____ theme script

---
+++ misc/switch-to-light-theme.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+lookandfeeltool -a org.kde.breeze.desktop
+
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+var allDesktops = desktops();
+for (i = 0; i < allDesktops.length; i++) {
+    d = allDesktops[i];
+    d.wallpaperPlugin = "org.kde.image";
+    d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+    d.writeConfig("Image", "file:///home/horseman/nix-config/misc/nixos_wallpaper_light_3840x2160.png");
+}'
