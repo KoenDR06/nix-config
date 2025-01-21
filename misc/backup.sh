690c23ecefab9a5db9a95eae666696060619f402
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-01-21 13:47:08 +0100

    backup system only saves file when hashes do not match

--- misc/backup.sh
+++ misc/backup.sh
@@ -1,5 +1,13 @@
 #!/run/current-system/sw/bin/zsh
 
 cd /home/horseman
+file=backups/$(date +%Y-%m-%d).tar
+tar -cf $file Documents
 
-tar -cf backups/$(date +%Y-%m-%d).tar Documents
+if grep -q $(sha256sum $file) backups/sum; then
+    echo 'hashes match, quitting'
+    rm $file
+    exit 0
+fi
+
+echo $(sha256sum ) > backups/sum
