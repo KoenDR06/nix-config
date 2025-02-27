6801c83f760ce4d97a8032d2d03d7b873a5cafb5
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-02-28 00:27:38 +0100

    Add wol timer to terra

--- machines/terra/configuration.nix
+++ machines/terra/configuration.nix
@@ -16,5 +16,24 @@
 
   networking.hostName = "terra";
 
+  systemd.timers."enable-wol" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "1m";
+      Unit = "enable-wol.service";
+    };
+  };
+
+  systemd.services."enable-wol" = {
+    script = ''
+      /home/horseman/nix-config/misc/startup.sh
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
+
   system.stateVersion = "24.11";
 }
