0a5efb050cc2ca8a249a57b6cd34bc040c334a00
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-04-08 12:47:38 +0200

    changed accept autosuggest keybinding to <C-y>

--- modules/apps/terminal/neovim.nix
+++ modules/apps/terminal/neovim.nix
@@ -120,6 +120,16 @@
           -- disable mason.nvim, use programs.neovim.extraPackages
           { "williamboman/mason-lspconfig.nvim", enabled = false },
           { "williamboman/mason.nvim", enabled = false },
+          {
+            "hrsh7th/nvim-cmp",
+            opts = function(_, opts)
+              local cmp = require("cmp")
+              opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<CR>"] = cmp.config.disable,
+              })
+            end,
+          },
         },
       })
     '';
