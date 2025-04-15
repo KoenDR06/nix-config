9d2fd3aba2f9fed7b92068c87700710eaba272d4
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-04-15 21:32:16 +0200

    open 3000 in firewall

--- machines/solis/configuration.nix
+++ machines/solis/configuration.nix
@@ -14,10 +14,9 @@
 
   horseman.username = "horseman";
 
-  networking = {
-    hostName = "solis";
-    firewall.allowedTCPPorts = [80 443];
-  };
+  networking.hostName = "solis";
+
+  networking.firewall.allowedTCPPorts = [3000];
 
   system.stateVersion = "24.11";
 }
