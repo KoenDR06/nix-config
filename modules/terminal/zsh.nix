752647703aca84601aeadd32c7c970943bb8753a
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-04-26 12:20:00 +0200

    nix fmt fix

--- modules/terminal/zsh.nix
+++ modules/terminal/zsh.nix
@@ -27,7 +27,7 @@ in {
 
       shellAliases = {
         clone-dotfiles = "cp -r /home/horseman/nix-config/config/dotfiles/.\* /home/horseman/";
-        rebuild = "function _rebuild(){clone-dotfiles && sudo nixos-rebuild switch --flake \"$@\" && nix fmt}; _rebuild";
+        rebuild = "function _rebuild(){clone-dotfiles && sudo nixos-rebuild switch --flake \"$@\" && nix fmt *}; _rebuild";
         update = "sudo nix flake update && rebuild";
         compose = "sudo docker compose";
         wolpc = "wakeonlan D8:5E:D3:A8:B1:0A";
